a476c4c6943eea0d004ffdebbec83d3a25f19fbb
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-02-21 15:34:23 +1300

    chore(web): view deploy URL on homepage

--- apps/web/app/routes/_public/index.tsx
+++ apps/web/app/routes/_public/index.tsx
@@ -1,13 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { env } from '~/utils/env';
 
 export const Route = createFileRoute('/_public/')({
   component: Home,
-})
+});
 
 function Home() {
   return (
     <div className="p-2">
       <h3>Welcome Home!!!</h3>
+      <p>{env.PUBLIC_DEPLOY_URL}</p>
     </div>
-  )
+  );
 }
