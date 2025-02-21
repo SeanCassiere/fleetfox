8a901e9b13e48cf452823c65b2dd4ec083aadccb
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-02-21 18:48:40 +1300

    refactor(web): use postcss instead of the vite plugin

--- apps/web/app/routes/_public.tsx
+++ apps/web/app/routes/_public.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createFileRoute, Outlet, Link } from '@tanstack/react-router';
+import { Button } from '~/components/ui/button';
 
 export const Route = createFileRoute('/_public')({
   component: RouteComponent,
@@ -8,25 +9,42 @@ export const Route = createFileRoute('/_public')({
 function RouteComponent() {
   return (
     <React.Fragment>
-      <div className="p-2 flex gap-2 text-lg">
-        <Link
-          to="/"
-          activeProps={{
-            className: 'font-bold',
-          }}
-          activeOptions={{ exact: true }}
-        >
-          Home
-        </Link>
-        <Link
-          to="/posts"
-          activeProps={{
-            className: 'font-bold',
-          }}
-        >
-          Posts
-        </Link>
-      </div>
+      <nav className="flex justify-between items-center">
+        <ul className="p-2 flex gap-2 text-lg">
+          <li>
+            <Link
+              to="/"
+              activeProps={{
+                className: 'font-bold',
+              }}
+              activeOptions={{
+                exact: true,
+                includeHash: false,
+                includeSearch: false,
+              }}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/posts"
+              activeProps={{
+                className: 'font-bold',
+              }}
+            >
+              Posts
+            </Link>
+          </li>
+        </ul>
+        <ul className="p-2 flex gap-2 text-lg">
+          <li>
+            <Button asChild>
+              <Link to="/app">Go to app</Link>
+            </Button>
+          </li>
+        </ul>
+      </nav>
       <hr />
       <Outlet />
     </React.Fragment>
