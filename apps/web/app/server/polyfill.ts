fd6ab2c8d2ab1afbc728218d5df5c9e03d8d75cb
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-03-07 17:54:21 +1300

    chore(web): comment out any polyfill code

--- apps/web/app/server/polyfill.ts
+++ apps/web/app/server/polyfill.ts
@@ -1,22 +1,22 @@
-import { env } from '~/lib/env';
+// import { env } from '~/lib/env';
 
-const deployEnvs: Array<(typeof env)['VITE_WEB_MODE']> = [
-  'production',
-  'deploy-preview',
-];
+// const deployEnvs: Array<(typeof env)['VITE_WEB_MODE']> = [
+//   'production',
+//   'deploy-preview',
+// ];
 
-/**
- * Any polyfill that needs to be added to the server should be added here.
- */
-export async function serverPolyfill() {
-  if (deployEnvs.includes(env.VITE_WEB_MODE)) {
-    if (!globalThis.crypto) {
-      console.info('Server polyfill for crypto is not needed in production.');
-    }
-  }
+// /**
+//  * Any polyfill that needs to be added to the server should be added here.
+//  */
+// export async function serverPolyfill() {
+//   if (deployEnvs.includes(env.VITE_WEB_MODE)) {
+//     if (!globalThis.crypto) {
+//       console.info('Server polyfill for crypto is not needed in production.');
+//     }
+//   }
 
-  if (!globalThis.crypto) {
-    const { Crypto } = await import('@peculiar/webcrypto');
-    globalThis.crypto = new Crypto();
-  }
-}
+//   if (!globalThis.crypto) {
+//     const { Crypto } = await import('@peculiar/webcrypto');
+//     globalThis.crypto = new Crypto();
+//   }
+// }
