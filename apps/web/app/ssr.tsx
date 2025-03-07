22181bb2a3bd39d620e53805fd60bb166626d708
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-03-07 18:14:24 +1300

    refactor(web): remove `globalThis.crypto` polyfill (#7)

--- apps/web/app/ssr.tsx
+++ apps/web/app/ssr.tsx
@@ -6,12 +6,8 @@ import {
 import { getRouterManifest } from '@tanstack/react-start/router-manifest';
 
 import { createRouter } from './router';
-import { serverPolyfill } from './server/polyfill';
 
 export default createStartHandler({
   createRouter,
   getRouterManifest,
-})(async (args) => {
-  await serverPolyfill();
-  return defaultStreamHandler(args);
-});
+})(defaultStreamHandler);
